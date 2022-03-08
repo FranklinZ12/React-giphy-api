e266acc16b2c39f749367a4ce3676ee7b1c6ee97
FranklinZ12 <franklinq8@gmail.com>
2022-03-08 12:40:50 -0500

    inicio de la aplicacion

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { Fragment } from "react";
+import GifExpertApp from "./components/GifExpertApp";
+import './index.css';
+function App() {
+  return (
+    <Fragment className="App">
+      <GifExpertApp />
+    </Fragment>
+  );
+}
+
+export default App;
---
+++ src/components/GifExpertApp.jsx
@@ -0,0 +1,12 @@
+import React, { Fragment } from 'react'
+
+const GifExpertApp = () => {
+    return (
+        <Fragment>
+            <h2>GifExpertApp</h2>
+            <hr />
+        </Fragment>
+    )
+}
+
+export default GifExpertApp
